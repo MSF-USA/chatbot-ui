e39d4c15e57913eb5af36a22fcb8ba94db92c95d
Mitchell.Smith <mitchell.smith@newyork.msf.org>
2023-09-28 09:46:35 -0400

    changed backend config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { SessionProvider } from "next-auth/react"
 
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
@@ -9,16 +10,18 @@ import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-function App({ Component, pageProps }: AppProps<{}>) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps<{}>) {
   const queryClient = new QueryClient();
 
   return (
+    <SessionProvider session={session}>
     <div className={inter.className}>
       <Toaster />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
     </div>
+    </SessionProvider>
   );
 }
 
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: '8776ae52da667c01d5f7',
+      clientSecret: 'aa384e44db3b568b3b5a38700cfe2eac86779e0d',
+    }),
+    // ...add more providers here
+  ],
+}
+export default NextAuth(authOptions)
