34bd58c6792e0a9082d9775c2293d79d3f75b9d2
Mitchell.Smith <mitchell.smith@newyork.msf.org>
2023-09-29 12:07:06 -0400

    add default client id

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,20 +1,19 @@
-import NextAuth from "next-auth"
-import AzureADProvider from "next-auth/providers/azure-ad";
-
+import NextAuth from 'next-auth';
+import AzureADProvider from 'next-auth/providers/azure-ad';
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     AzureADProvider({
-        clientId: process.env.AZURE_AD_CLIENT_ID,
-        clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
-        tenantId: process.env.AZURE_AD_TENANT_ID,
-      }),
+      clientId: process.env.AZURE_AD_CLIENT_ID || '',
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET || '',
+      tenantId: process.env.AZURE_AD_TENANT_ID || '',
+    }),
     // ...add more providers here
   ],
   pages: {
-    signIn: '/auth/signin'
-  }
-}
+    signIn: '/auth/signin',
+  },
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
