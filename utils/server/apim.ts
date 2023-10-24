07f4c4e1a9d296c67dea1d21eed0de4f41c2e90b
Blaze Burgess <blaze.i.burgess@gmail.com>
2023-10-24 14:21:20 -0400

    Add separate APIM handler for api queries
    Ultimately we want to use this primarily, but I want to keep the codebase flexible. So putting this in its own location rather than changing the OpenAI 'plugin'

---
+++ utils/server/apim.ts
@@ -0,0 +1,44 @@
+export class APIMError extends Error {
+    type: string;
+    param: string;
+    code: string;
+
+    constructor(message: string, type: string, param: string, code: string) {
+        super(message);
+        this.name = 'APIMError';
+        this.type = type;
+        this.param = param;
+        this.code = code;
+    }
+}
+
+export const makeAPIMRequest = async (
+    url: string, accessToken: string, method: string, body: any
+) => {
+    const res = await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${ accessToken }`
+        },
+        method,
+        body: JSON.stringify(body),
+    });
+
+    if (res.status === 401) {
+        return new Response(res.body, {
+            status: 500,
+            headers: res.headers,
+        });
+    } else if (res.status !== 200) {
+        console.error(
+            `APIM API returned an error ${
+            res.status
+            }: ${await res.text()}`,
+        );
+        throw new Error('APIM API returned an error');
+    }
+
+    const json = await res.json();
+    console.log(json)
+    return json.message;
+}
