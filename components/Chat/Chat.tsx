3d86f151505fa51a11a0424722a3ee442a546c4e
Blaze Burgess <blaze.i.burgess@gmail.com>
2023-10-30 13:10:12 -0400

    Abstract out condition to show splash screen, account for apim hosting
    Included a new condition to determine when to show the splash screen in the Chat component. This change was made to ensure that the splash screen only shows up if there is no API key and the application host type is not 'apim'.

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -33,6 +33,7 @@ import { ModelSelect } from './ModelSelect';
 import { SystemPrompt } from './SystemPrompt';
 import { TemperatureSlider } from './Temperature';
 import { MemoizedChatMessage } from './MemoizedChatMessage';
+import {OPENAI_API_HOST_TYPE} from "@/utils/app/const";
 
 interface Props {
   stopConversationRef: MutableRefObject<boolean>;
@@ -468,10 +469,11 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       }
     };
   }, [messagesEndRef]);
+  const showSplash = !(apiKey || serverSideApiKeyIsSet) && OPENAI_API_HOST_TYPE !== 'apim'
 
   return (
     <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#343541]">
-      {!(apiKey || serverSideApiKeyIsSet) ? (
+      {(showSplash) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
             Welcome to Chatbot UI
