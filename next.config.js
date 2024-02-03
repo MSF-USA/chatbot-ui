aae66bade40faef390bdbc79dd4c12cb3cb22e97
Mitchell.Smith <mitchell.smith@newyork.msf.org>
2024-02-02 22:33:50 -0500

    fix git ignore for pwa sw

--- next.config.js
+++ next.config.js
@@ -26,7 +26,7 @@ const nextConfig = {
 
 const withPWA = require("next-pwa")({
   dest: "public", // Destination directory for the PWA files
-  disable: false, //process.env.NODE_ENV === "development", // Disable PWA in development mode
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
   // register: true, // Register the PWA service worker
   // skipWaiting: true, // Skip waiting for service worker activation
   reloadOnOnline: true
