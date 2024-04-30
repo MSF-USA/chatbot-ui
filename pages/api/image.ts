7af84a31e268e0f760de7ead75f162ac233945e5
blazeburgess <blaze.i.burgess@gmail.com>
2024-04-30 13:41:27 -0400

    Implement image upload function in API
    The implementation of the image upload function has been accomplished in the page's api/image.ts. With the help of AzureBlobStorage client, images get uploaded to the desired container. Hashing has also been introduced to create a unique name for each file. Upon successful upload, the function sets HTTP status to 200 and responds with the URI of the uploaded image.

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -1,25 +1,35 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import {AzureBlobStorage} from "@/utils/server/blob";
 import {getEnvVariable} from "@/utils/app/env";
+import Hasher from "@/utils/app/hash";
 
 const page = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         const imageData = req.body; // get image data from request body
 
+        // get filename from params
+        const filename = req.query.filename as string;
+
         // TODO: Implement the uploader function that will handle the upload to your blob storage.
-        const uploadImageToBlobStorage = (data: any) => {
+        const uploadImageToBlobStorage = async (data: any) => {
             let blobStorageClient: AzureBlobStorage = new AzureBlobStorage(
                 getEnvVariable('AZURE_BLOB_STORAGE_NAME'),
                 getEnvVariable('AZURE_BLOB_STORAGE_KEY'),
                 getEnvVariable('AZURE_BLOB_STORAGE_IMAGE_CONTAINER')
             );
-            console.log('TODO: implement upload to blob storage');
+
+            const hashedFileContents = Hasher.sha256(data).slice(0, 200);
+            const extension = filename.split('.').pop();
+            return await blobStorageClient.upload(
+                `${hashedFileContents}.${extension}`,
+                data
+            )
         }
 
         // Call uploader function
-        uploadImageToBlobStorage(imageData);
+        const imageURI: string = await uploadImageToBlobStorage(imageData);
 
-        res.status(200).json({message: 'Image uploaded'});
+        res.status(200).json({message: 'Image uploaded', uri: imageURI});
     } else {
         // If it's not a POST request, return 405 - Method Not Allowed
         res.setHeader('Allow', ['POST']);
