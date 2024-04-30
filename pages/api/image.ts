1c70114ff04a99d496e45332549ba7d02d3a41b9
blazeburgess <blaze.i.burgess@gmail.com>
2024-04-30 12:58:34 -0400

    Implement Azure Blob Storage setup in image API
    Introduced the AzureBlobStorage client within the uploadImageToBlobStorage function in image API. The client is instantiated with necessary environment variables such as storage name, key, and image container. This setup lays the groundwork for enabling image uploads to Azure Blob Storage in subsequent updates.

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -1,12 +1,18 @@
 import {NextApiRequest, NextApiResponse} from 'next'
+import {AzureBlobStorage} from "@/utils/server/blob";
+import {getEnvVariable} from "@/utils/app/env";
 
 const page = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         const imageData = req.body; // get image data from request body
-        console.log(imageData)
 
         // TODO: Implement the uploader function that will handle the upload to your blob storage.
         const uploadImageToBlobStorage = (data: any) => {
+            let blobStorageClient: AzureBlobStorage = new AzureBlobStorage(
+                getEnvVariable('AZURE_BLOB_STORAGE_NAME'),
+                getEnvVariable('AZURE_BLOB_STORAGE_KEY'),
+                getEnvVariable('AZURE_BLOB_STORAGE_IMAGE_CONTAINER')
+            );
             console.log('TODO: implement upload to blob storage');
         }
 
