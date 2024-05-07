8e9be7d5cfbc843ac910e9184c048ca7dcc2d12a
blazeburgess <blaze.i.burgess@gmail.com>
2024-05-07 15:25:43 -0400

    Refactor ChatInputImage for upload enhancements
    Extended the functionality within the `onImageUpload` and `ChatInputImage` functions to handle images in the chat better. Now it also checks whether the file dialog has been canceled and changes the submit type back to 'text', stores image previews, and takes an image prompt string. The aim is to make the image upload process more intuitive and user-friendly while maintaining the image preview state.

--- components/Chat/ChatInputImage.tsx
+++ components/Chat/ChatInputImage.tsx
@@ -1,10 +1,25 @@
 import ImageIcon from "@/components/Icons/image";
-import React, {MutableRefObject, useRef} from "react";
+import React, {Dispatch, MutableRefObject, SetStateAction, useRef} from "react";
+import {ImageMessageContent, TextMessageContent} from "@/types/chat";
 
 
-const onImageUpload = (event: React.ChangeEvent<any>, setContent: any) => {
+const onImageUpload = (
+    event: React.ChangeEvent<any>,
+    setContent: Dispatch<SetStateAction<string | Array<TextMessageContent | ImageMessageContent>>>,
+    prompt: string,
+    setFilePreviews:  Dispatch<SetStateAction<string[]>>,
+    setSubmitType: Dispatch<SetStateAction<string>>,
+) => {
     event.preventDefault();
     const file = event.target.files[0];
+
+    // User cancelled the file dialog, reset to 'text' submit type
+    if (!file) {
+        setSubmitType('text');
+        return;
+    } else {
+        setSubmitType("image");
+    }
 
     const formData = new FormData();
     formData.append("file", file);
@@ -21,14 +36,21 @@ const onImageUpload = (event: React.ChangeEvent<any>, setContent: any) => {
                 setContent([
                     {
                         type: 'text',
-                        text: 'Describe this image:'
+                        text: prompt,
                     },
                     {
                         type: 'image_url',
                         image_url: {url: data.uri}
                     },
                 ])
-
+                setFilePreviews(prevFilePreviews => {
+                    if (Array.isArray(prevFilePreviews)) {
+                        prevFilePreviews.push(base64String)
+                        return prevFilePreviews
+                    } else {
+                        return [base64String]
+                    }
+                })
             })
         })
 
@@ -43,7 +65,14 @@ const onImageUploadButtonClick = (event: React.ChangeEvent<any>, fileInputRef: M
     fileInputRef.current.click();
 }
 
-const ChatInputImage = ({setSubmitType, setContent}: any) => {
+const ChatInputImage = (
+    {
+        setSubmitType,
+        setContent,
+        prompt,
+        setFilePreviews
+    }: any
+) => {
     const imageInputRef: MutableRefObject<any> = useRef(null);
 
 
@@ -52,17 +81,21 @@ const ChatInputImage = ({setSubmitType, setContent}: any) => {
             type="file"
             ref={imageInputRef}
             style={{display: "none"}}
-            onChange={(event) => onImageUpload(event, setContent)}
+            onChange={(event) => {
+                onImageUpload(event, setContent, prompt, setFilePreviews, setSubmitType)
+            }}
             accept={"image/*"}
         />
-        <button onClick={(e) => {
-            setSubmitType('image');
-            onImageUploadButtonClick(e, imageInputRef)
-        }}>
-            <ImageIcon className="h-5 w-5"/>
+        <button
+            onClick={(e) => {
+                onImageUploadButtonClick(e, imageInputRef)
+            }}
+            className={""}
+        >
+            <ImageIcon className="bg-[#343541] rounded h-5 w-5"/>
             <span className="sr-only">Add image</span>
         </button>
     </>
 }
 
-export default ChatInputImage
+export default ChatInputImage;
