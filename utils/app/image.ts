3c3f54ff37066f90dabb02d35001203f80e8b050
Blaze Burgess <blaze.i.burgess@gmail.com>
2024-05-12 13:18:13 -0400

    Improve efficiency of image processing
    Implemented a more efficient server-side method for processing images in the image.ts file of the app utilities. In case of buffer errors, the less efficient but client-side compatible method is included to ensure seamless operation. The client-side method was previously the only implementation.

--- utils/app/image.ts
+++ utils/app/image.ts
@@ -6,9 +6,16 @@ export const getBase64FromImageURL = async (imageUrl: string): Promise<string> =
             throw new Error('Network response was not ok');
         }
 
-        const arrayBuffer = await response.arrayBuffer();
-        // @ts-ignore
-        return String.fromCharCode(...new Uint8Array(arrayBuffer));
+        try {
+            // More efficient server-side method
+            const buffer = Buffer.from(await response.arrayBuffer());
+            return buffer.toString();
+        } catch (bufferError) {
+            // less efficient, client-side compatible method
+            const arrayBuffer = await response.arrayBuffer();
+            // @ts-ignore
+            return String.fromCharCode(...new Uint8Array(arrayBuffer));
+        }
     } catch (error) {
         throw new Error(`Error fetching the image: ${error}`);
     }
