41e6127f09ef2f6c21b9658569a049e89c9d4f74
Blaze Burgess <blaze.i.burgess@gmail.com>
2024-06-02 18:37:12 -0400

    Refactor UserFileHandler tests
    Reinstate tests for constructor with local and remote file data, as well as unsupported file type. These were previously removed by accident when debugging an error using ai.
    
    Update extractText test to ignore type errors.

--- __tests__/utils/app/userFile.test.ts
+++ __tests__/utils/app/userFile.test.ts
@@ -10,7 +10,29 @@ describe('UserFileHandler', () => {
   };
 
   describe('constructor', () => {
-    // ... (constructor tests remain the same)
+    it('should create an instance with local file data', () => {
+      const fileData = new Blob(['test'], { type: 'text/plain' });
+      // @ts-ignore
+      const fileHandler = new UserFileHandler(fileData, {...validFileTypes, plain: true});
+      expect(fileHandler).toBeInstanceOf(UserFileHandler);
+      expect(fileHandler['fileLocation']).toBe('local');
+      expect(fileHandler['fileType']).toBe('plain');
+    });
+
+    it('should create an instance with remote file data', () => {
+      const fileData = 'https://example.com/test.txt';
+      // @ts-ignore
+      const fileHandler = new UserFileHandler(fileData, validFileTypes);
+      expect(fileHandler).toBeInstanceOf(UserFileHandler);
+      expect(fileHandler['fileLocation']).toBe('remote');
+      expect(fileHandler['fileType']).toBe('txt');
+    });
+
+    it('should throw an error for unsupported file type', () => {
+      const fileData = 'test.unsupported';
+      // @ts-ignore
+      expect(() => new UserFileHandler(fileData, validFileTypes)).toThrowError('Unsupported file type: unsupported');
+    });
   });
 
   describe('extractText', () => {
@@ -43,6 +65,7 @@ describe('UserFileHandler', () => {
         blob: vi.fn().mockResolvedValue(new Blob(['Test content'], { type: 'text/plain' })),
       });
       global.fetch = mockFetch;
+      // @ts-ignore
       const fileHandler = new UserFileHandler(fileData, validFileTypes);
 
       const result = await fileHandler.extractText();
