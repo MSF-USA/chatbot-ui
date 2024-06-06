daa697b8744e5b9304d23cfa865aeac8d393f80f
Blaze Burgess <blaze.i.burgess@gmail.com>
2024-06-06 11:20:29 -0400

    Restrict vitest config for util tests
    Add test configuration to vitest.config.ts to run tests in __tests__/utils/ with
    a Node.js environment. This allows running tests for utility functions without
    relying on a browser environment. Part of an attempt to split frontend (jsdom) and backend (node) testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,16 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  test: {
+    include: [
+      '__tests__/utils/**/*.test.ts',
+    ],
+    environment: 'node'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
     },
   },
+
 });
