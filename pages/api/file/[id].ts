bbd3f1cb8bf3afa61ea192c2f6fd9c9e7c455c85
blazeburgess <blaze.i.burgess@gmail.com>
2024-06-25 12:04:49 -0400

    Update file retrieval to include file type verification
    The change expands the server-side file retrieval method to support both 'image' and 'file' types in addition to the default type. The validation for file identifiers has also been updated to cater for different types.

--- pages/api/file/[id].ts
+++ pages/api/file/[id].ts
@@ -1,5 +1,5 @@
 import {NextApiRequest, NextApiResponse} from "next";
-import {getBlobBase64String} from "@/utils/server/blob";
+import {AzureBlobStorage, BlobProperty, getBlobBase64String} from "@/utils/server/blob";
 import {getToken} from "next-auth/jwt";
 
 /**
@@ -8,8 +8,9 @@ import {getToken} from "next-auth/jwt";
  * @param {string} id - The identifier to validate.
  * @returns {boolean} True if the identifier is a valid SHA-256 hash, false otherwise.
  */
-const isValidSha256Hash = (id: string): boolean => {
+const isValidSha256Hash = (id: string | string[] | undefined): boolean => {
   if (typeof id !== 'string' || id.length < 1) {
+    console.error(`Invalid id type '${typeof id}' for object: ${JSON.stringify(id)}`)
     return false;
   }
   const idParts: string[] = id.split('.');
@@ -28,22 +29,40 @@ const isValidSha256Hash = (id: string): boolean => {
 
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { id } = req.query;
+  const { id, fileType: requestedFileType } = req.query;
 
-  if (!isValidSha256Hash(id as string)) {
+  if (!isValidSha256Hash(id)) {
     res.status(400).json({ error: 'Invalid file identifier' });
     return;
   }
 
+  let fileType: 'image' | 'file' = 'file';
+  if (requestedFileType === 'image' || requestedFileType === 'file') {
+    fileType = requestedFileType;
+  }
+
+
   // @ts-ignore
   const token: JWT = await getToken({req});
   const userId: string = token.userId ?? 'anonymous';
+  const remoteFilepath = `${userId}/uploads/${fileType}s`;
 
 
   try {
-    const base64String: string = await getBlobBase64String(userId, id as string);
+    if (fileType === 'image') {
+      const base64String: string = await getBlobBase64String(userId, id as string);
 
-    res.status(200).json({ base64Url: base64String });
+      res.status(200).json({base64Url: base64String});
+    } else if (fileType === 'file') {
+      const blobStorage = new AzureBlobStorage()
+      const blob: Buffer = await (blobStorage.get(
+          `${remoteFilepath}/${id}`,
+          BlobProperty.BLOB
+      ) as Promise<Buffer>)
+      res.status(200).send(blob.toString())
+    } else {
+      throw new Error(`Invalid fileType requested: ${fileType}`)
+    }
   } catch (error) {
     console.error('Error retrieving blob:', error);
     res.setHeader('Cache-Control', 's-maxage=43200, stale-while-revalidate');
