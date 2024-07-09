5f9b4c7e67dbbac89ee16bb48e5f65dea0eb39c5
Mitchell Smith <mitchellryansmith@gmail.com>
2024-07-09 10:46:47 -0400

    added more prompts

--- components/Chat/prompts.ts
+++ components/Chat/prompts.ts
@@ -1,11 +1,15 @@
 import {
   IconAlignJustified,
   IconBulb,
+  IconCalendar,
+  IconCheckbox,
   IconDatabase,
   IconHelpHexagon,
   IconLanguage,
   IconMail,
+  IconPresentation,
   IconSettingsAutomation,
+  IconTextScan2,
 } from '@tabler/icons-react';
 
 export const suggestedPrompts = [
@@ -45,5 +49,28 @@ export const suggestedPrompts = [
     prompt:
       "I'm not sure where to start with how I might need assistance. Can you suggest some ways in which you can help?",
     icon: IconHelpHexagon,
+  },
+  {
+    title: 'Revise Text',
+    prompt:
+      'I have a chunk of text which needs revision. Please examine the text and revise it to the best of your ability.',
+    icon: IconTextScan2,
+  },
+  {
+    title: 'Create A Presentation',
+    prompt:
+      'I need to create a compelling presentation on a specific subject. Please help me outline or draft it.',
+    icon: IconPresentation,
+  },
+  {
+    title: 'Generate Meeting Agenda',
+    prompt: 'Can you help me create an agenda for an upcoming meeting?',
+    icon: IconCalendar,
+  },
+  {
+    title: 'Develop A Survey',
+    prompt:
+      'Can you help me design a survey to gather feedback or information on a particular topic?',
+    icon: IconCheckbox,
   },
 ];
