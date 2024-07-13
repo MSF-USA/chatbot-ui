387157227b530b38a78d18b4764e7194341fe8e9
Blaze Burgess <blaze.i.burgess@gmail.com>
2024-07-12 21:20:04 -0400

    Optimize chunk summarization and outline code for multiple file type handling
    Introduce file type detection and corresponding document loaders to enable processing of various file formats including PDF, DOCX, and plain text. Improve chunk summarization by focusing on relevance to the user's prompt and capturing the original text's nuance. Optimize performance by processing chunks concurrently with error handling.

--- utils/app/documentSummary.ts
+++ utils/app/documentSummary.ts
@@ -1,10 +1,13 @@
-import {makeAPIMRequestWithRetry} from "@/utils/server/apim";
-import {getToken} from "next-auth/jwt";
 import {JWT} from "next-auth";
-import {ApimChatResponseDataStructure} from "@/types/apim";
 import {APIM_CHAT_ENDPONT, DEFAULT_SYSTEM_PROMPT, OPENAI_API_HOST, OPENAI_API_VERSION} from "@/utils/app/const";
 import OpenAI from "openai";
 import {OpenAIStream} from "ai";
+import {lookup} from "mime-types";
+import mammoth from 'mammoth';
+import {DocxLoader} from "@langchain/community/document_loaders/fs/docx";
+import pdfParse from 'pdf-parse'
+
+
 
 interface parseAndQueryFilterOpenAIArguments {
     file: File;
@@ -14,19 +17,75 @@ interface parseAndQueryFilterOpenAIArguments {
     maxLength?: number;
 }
 
+async function loadPDF(file: File): Promise<string> {
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    // const data = await pdfParse(buffer);
+    // return data.text;
+    return "";
+}
+
+async function loadDOCX(file: File): Promise<string> {
+    const arrayBuffer = await file.arrayBuffer();
+    const uint8Array = new Uint8Array(arrayBuffer);
+    try {
+        const result = await mammoth.extractRawText({ arrayBuffer: uint8Array });
+        return result.value;
+    } catch (error: any) {
+        console.error(`Error processing the DOCX file: ${error.message}`);
+        throw error;
+    }
+}
+
+async function loadDocument(file: File): Promise<string> {
+    let text, content, loader;
+    const mimeType = lookup(file.name) || 'application/octet-stream';
+
+    switch (true) {
+        case mimeType.startsWith('application/pdf'):
+            text = await loadPDF(file);
+            // loader = new PDFLoader(file);
+            // content = await loader.load();
+            // text = content[0].pageContent
+
+            break;
+        case mimeType.startsWith('application/vnd.openxmlformats-officedocument.wordprocessingml.document'):
+            loader = new DocxLoader(file);
+            content = await loader.load();
+            text = content[0].pageContent
+            break;
+        case mimeType.startsWith('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'):
+            throw new Error("Not supported file type");
+        case mimeType.startsWith('application/vnd.openxmlformats-officedocument.presentationml.presentation'):
+            throw new Error("Not supported file type");
+        case mimeType.startsWith('application/epub+zip'):
+            throw new Error("Not supported file type");
+        case mimeType.startsWith('text/plain') || mimeType.startsWith('text/') || mimeType.startsWith('application/csv')
+        || mimeType.startsWith('application/json') || mimeType.startsWith('application/xhtml+xml'):
+        default:
+            try {
+                text = await file.text()
+            } catch (error) {
+                console.error(`Could not parse text from ${file.name}`);
+                throw error;
+            }
+    }
+    return text;
+}
+
 async function summarizeChunk(
   azureOpenai: OpenAI,
   modelId: string,
   prompt: string,
   chunk: string
 ): Promise<string> {
-    const summaryPrompt: string = `Summarize the following text with relevance to the prompt: ${prompt}\n\n\`\`\`text\n${chunk}\n\`\`\``;
+    const summaryPrompt: string = `Summarize the following text with relevance to the prompt, but keep enough details to maintain the tone, character, and content of the original. If nothing is relevant, then return an empty string:\n\n\`\`\`prompt\n${prompt}\`\`\`\n\n\`\`\`text\n${chunk}\n\`\`\``;
     const chunkSummary = await azureOpenai.chat.completions.create({
         model: modelId,
         messages: [
             {
                 role: "system",
-                content: "You are an AI Text summarizer. You take the prompt of a user and rather than conclusively answering, you pull together all the relevant information for that prompt in a particular chunk of text and reshape that into brief statements capturing the nuanced intent of the original text."
+                content: "You are an AI Text summarizer. You take the prompt of a user and rather than conclusively answering, you pull together all the relevant information for that prompt in a particular chunk of text and reshape that into brief statements capturing the nuanced intent of the original text. Focus on how the provided text answers the user's question. If it doesn't then briefly make that clear."
             },
             {
                 role: "user",
@@ -43,8 +102,8 @@ async function summarizeChunk(
 export async function parseAndQueryFileOpenAI(
   {file, prompt, token, modelId, maxLength = 6000}: parseAndQueryFilterOpenAIArguments
 ): Promise<ReadableStream<any>> {
-    const fileContent = await file.text();
-    const chunks: string[] = splitIntoChunks(fileContent);
+    const fileContent = await loadDocument(file);
+    let chunks: string[] = splitIntoChunks(fileContent);
 
     const openAIArgs: any = {
         baseURL: `${OPENAI_API_HOST}/${APIM_CHAT_ENDPONT}/deployments/${modelId}`,
@@ -63,19 +122,27 @@ export async function parseAndQueryFileOpenAI(
     let combinedSummary: string = "";
 
     while (chunks.length > 0) {
-        const summaries: string[] = [];
+        const chunkPromises = chunks.map(chunk =>
+          summarizeChunk(azureOpenai, modelId, prompt, chunk)
+            .catch(error => {
+                console.error(error);
+                return null;
+            })
+        );
 
-        for (const chunk of chunks) {
-            const summary = await summarizeChunk(azureOpenai, modelId, prompt, chunk);
-            summaries.push(summary);
+        const summaries = await Promise.all(chunkPromises);
+        const validSummaries = summaries.filter(summary => summary !== null);
 
-            if (summaries.join(' ').length >= maxLength) {
+        let batchSummary = '';
+        for (const summary of validSummaries) {
+            if ((batchSummary + summary).length > maxLength) {
                 break;
             }
+            batchSummary += summary + ' ';
         }
 
-        combinedSummary += summaries.join(' ');
-        chunks.splice(0, summaries.length);
+        combinedSummary += batchSummary;
+        chunks = chunks.slice(validSummaries.length);
     }
 
     const finalPrompt: string = `${combinedSummary}\n\nUser prompt: ${prompt}`;
@@ -85,7 +152,7 @@ export async function parseAndQueryFileOpenAI(
         messages: [
             {
                 role: "system",
-                content: DEFAULT_SYSTEM_PROMPT
+                content: "You are a document analyzer AI Assistant. You perform all tasks the user requests of you, careful to make sure you are responding to the spirit and intentions behind their request. You make it clear how your responses relate to the base text that you are processing and provide your responses in markdown format when special formatting is necessary. Understand that you are analyzing text that you have previously summarized, so make sure your response is an amalgamation of your impressions over each chunk."
             },
             {
                 role: "user",
@@ -101,7 +168,7 @@ export async function parseAndQueryFileOpenAI(
     return stream;
 }
 
-function splitIntoChunks(text: string, chunkSize: number = 2000): string[] {
+function splitIntoChunks(text: string, chunkSize: number = 6000): string[] {
     const chunks: string[] = [];
     for (let i = 0; i < text.length; i += chunkSize) {
         chunks.push(text.slice(i, i + chunkSize));
