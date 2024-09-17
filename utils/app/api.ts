5298fc22674e79897d6dd32173d4010b9fedff0b
Blaze Burgess <blaze.i.burgess@gmail.com>
2024-09-17 12:08:00 -0400

    Refactor getEndpoint to use an argument object.
    Not necessary for this branch but other changes assumed this change as a dependency.

--- utils/app/api.ts
+++ utils/app/api.ts
@@ -1,6 +1,10 @@
 import { Plugin, PluginID } from '@/types/plugin';
 
-export const getEndpoint = (plugin: Plugin | null) => {
+interface GetEndpointArgs {
+  plugin?: Plugin | undefined | null;
+}
+
+export const getEndpoint = ({plugin}: GetEndpointArgs) => {
   if (plugin?.id === PluginID.GOOGLE_SEARCH) {
     throw new Error('Google Plugin no longer supported.')
   }
