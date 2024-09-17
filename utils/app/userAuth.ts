742b49a76ed2692612b2f343daa1aa6c1e63594c
Blaze Burgess <blaze.i.burgess@gmail.com>
2024-09-17 11:42:56 -0400

    Refactor email domain check for US-based users
    Comment out the old check for 'newyork' in user email and replace it with 'msf.org' for temporary testing purposes. This adjustment is to see if new file uploads work and are splitting correctly.

--- utils/app/userAuth.ts
+++ utils/app/userAuth.ts
@@ -1,7 +1,8 @@
 import {Session} from "next-auth";
 
 const isUSBased = (email: string): boolean => {
-  return email?.toLowerCase().indexOf('newyork') >= 0;
+  // return email?.toLowerCase().indexOf('newyork') >= 0;
+  return email?.toLowerCase().indexOf('msf.org') >= 0; //temporary value for testing in dev
 }
 
 export const userAuthorizedForFileUploads = (user: Session["user"] | undefined): boolean => {
