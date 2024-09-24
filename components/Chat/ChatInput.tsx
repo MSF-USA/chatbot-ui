5b6648761c372bbe068ae16c4def34a527689d99
Blaze Burgess <blaze.i.burgess@gmail.com>
2024-09-24 17:13:39 -0400

    Comment out voice input while development is still happening

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -453,8 +453,8 @@ export const ChatInput = ({
             setFileFieldValue={setFileFieldValue}
             setImageFieldValue={setImageFieldValue}
           />
-          <ChatInputVoiceCapture
-          />
+          {/*<ChatInputVoiceCapture*/}
+          {/*/>*/}
 
           <div
             className="relative mx-2 max-w-[900px] flex w-full flex-grow flex-col rounded-md border border-black/10 bg-white shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:border-gray-900/50 dark:bg-[#40414F] dark:text-white dark:shadow-[0_0_15px_rgba(0,0,0,0.10)] sm:mx-4">
