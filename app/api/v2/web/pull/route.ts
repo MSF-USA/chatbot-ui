7228d49a8094d27d1ecdeaf2967f43e315eab1e7
Blaze Burgess <blaze.i.burgess@gmail.com>
2024-10-12 22:44:39 -0400

    Replace custom code with webpage service
    See previous commit for reasoning, but simplifies endpoint logic while returning the same format.

--- app/api/v2/web/pull/route.ts
+++ app/api/v2/web/pull/route.ts
@@ -1,60 +1,16 @@
-export const runtime = 'nodejs';
-
+import { fetchAndParseWebpage } from "@/services/webpageService";
 import { NextRequest, NextResponse } from 'next/server';
-import * as cheerio from 'cheerio';
+
+export const runtime = 'nodejs';
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
     const { url } = await req.json();
 
-    // Validate the URL
-    if (!url || typeof url !== 'string') {
-      return NextResponse.json({ error: 'Invalid URL' }, {
-        status: 400,
-      });
-    }
+    const content = await fetchAndParseWebpage(url);
 
-    let validatedUrl: URL;
-    try {
-      validatedUrl = new URL(url);
-      if (!['http:', 'https:'].includes(validatedUrl.protocol)) {
-        throw new Error('Invalid protocol');
-      }
-    } catch (error: any) {
-      return NextResponse.json({ error: 'Invalid URL format' }, {
-        status: 400,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
 
-    const resp = await fetch(
-      validatedUrl.toString(),
-      {headers: {
-        'User-Agent': process.env.USER_AGENT ?? 'MSF Assistant'
-      }}
-    );
-    if (!resp.ok) {
-      return NextResponse.json({ error: 'Failed to fetch the URL' }, {
-        status: resp.status,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
-
-    const html = await resp.text();
-
-    const $ = cheerio.load(html);
-
-    const title = $('title').text().trim() || 'No title found';
-    const host = validatedUrl.host;
-
-    $('script, iframe, style, link[rel="stylesheet"], noscript').remove();
-    const textContent = $('body').text();
-    const cleanText = textContent.replace(/\s+/g, ' ').trim();
-
-    // Combine title, host, and content
-    const finalContent = `Title: ${title}\nURL: ${host}\n\n${cleanText}`;
-
-    return NextResponse.json({ content: finalContent }, {
+    return NextResponse.json({ content }, {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     });
