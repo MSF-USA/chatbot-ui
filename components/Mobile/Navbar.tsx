a65948b7fb84bf3fc8a7f7ffd81cb2254b721a5d
Chris-Graham-MSF <christopher.graham@newyork.msf.org>
2024-12-09 09:21:27 -0500

    Refactors the mobile navbar to remove the misplaced conversation text at the top. The plus icon has also been repositioned for better viewing on mobile.

--- components/Mobile/Navbar.tsx
+++ components/Mobile/Navbar.tsx
@@ -9,19 +9,13 @@ interface Props {
 }
 
 export const Navbar: FC<Props> = ({
-  selectedConversation,
   onNewConversation,
 }) => {
   return (
     <nav className="flex w-full justify-between bg-[#171717] py-3 px-4">
-      <div className="mr-4"></div>
-
-      <div className="max-w-[240px] overflow-hidden text-ellipsis whitespace-nowrap">
-        {selectedConversation.name}
-      </div>
-
+      <div></div>
       <IconPlus
-        className="cursor-pointer hover:text-neutral-400 mr-8"
+        className="cursor-pointer hover:text-neutral-400 ml-auto"
         onClick={onNewConversation}
       />
     </nav>
