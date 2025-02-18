18b9704751748383fd336ee281aa712c61a6ca84
Blaze Burgess <blaze.i.burgess@gmail.com>
2025-02-18 13:19:49 -0500

    Refactor header handling in transcription service
    Updated header logic to support both API key and Bearer token authentication. This adds flexibility for different authentication methods when making transcription requests.
    
    With this change, both local dev and local beta are working again.

--- services/transcription/whisperTranscriptionService.ts
+++ services/transcription/whisperTranscriptionService.ts
@@ -76,14 +76,17 @@ export class WhisperTranscriptionService implements ITranscriptionService {
 
     try {
       const reqUrl: string = `${this.endpoint!.trim()}/openai/deployments/${this.deployment}/audio/transcriptions?api-version=2024-06-01`;
+      const headers = formHeaders;
+      if (this.apiKey) {
+        headers['api-key'] = this.apiKey;
+      } else {
+        headers['Authorization'] = `Bearer ${token}`;
+      }
       const response = await axios.post(
           reqUrl,
           formData,
           {
-            headers: {
-              'Authorization': `Bearer ${token}`,
-              ...formHeaders,
-            },
+            headers
           }
       );
 
