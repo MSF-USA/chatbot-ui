c226abf646d963f7cd4bc4d4fac1902b85713ae4
Mitchell Smith <mitchellryansmith@gmail.com>
2025-06-10 11:46:58 -0400

    trying to fix vite aliases for tests in docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
+import react from '@vitejs/plugin-react';
+
 import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  test: {
-    globals: true,
-    include: ['__tests__/dom/**/*.test.tsx'],
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.dom.ts'],
-  },
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
