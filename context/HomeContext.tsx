9d1833f5ce6163815558f77e31789a5054aa0752
Blaze Burgess <blaze.i.burgess@gmail.com>
2025-12-10 08:05:32 -0500

    Add HomeContext and HomeState to context folder
    Previously this was in the pages folder, which is now treated by nextjs as an api endpoint. Currently the old files still exist. This is just setup for the final fix.

---
+++ context/HomeContext.tsx
@@ -0,0 +1,35 @@
+import { Dispatch, createContext } from 'react';
+
+import { Session } from 'next-auth';
+
+import { ActionType } from '@/hooks/useCreateReducer';
+
+import { Conversation } from '@/types/chat';
+import { KeyValuePair } from '@/types/data';
+import { FolderType } from '@/types/folder';
+
+import { SettingsSection } from '@/components/Settings/types';
+
+import { HomeInitialState } from './HomeState';
+
+export interface HomeContextProps {
+  user?: Session['user'];
+  state: HomeInitialState;
+  dispatch: Dispatch<ActionType<HomeInitialState>>;
+  handleNewConversation: () => void;
+  handleCreateFolder: (name: string, type: FolderType) => void;
+  handleDeleteFolder: (folderId: string) => void;
+  handleUpdateFolder: (folderId: string, name: string) => void;
+  handleSelectConversation: (conversation: Conversation) => void;
+  handleUpdateConversation: (
+    conversation: Conversation,
+    data: KeyValuePair,
+  ) => void;
+  handleOpenSettings: (section?: SettingsSection) => void;
+  handleCloseSettings: () => void;
+  showChatbar: boolean;
+}
+
+const HomeContext = createContext<HomeContextProps>(undefined!);
+
+export default HomeContext;
