12036350a4441af440fd5a85d8c4a4788f66446b
Blaze Burgess <blaze.i.burgess@gmail.com>
2026-01-21 13:09:53 -0500

    Clear conversation name when deleting messages
    - Updated `updateConversation` in `useChatActions` to reset the conversation name (`name: ''`) along with clearing messages.
    - Promotes consistent behavior when resetting conversation data.

--- client/hooks/chat/useChatActions.ts
+++ client/hooks/chat/useChatActions.ts
@@ -48,6 +48,7 @@ export function useChatActions({
     ) {
       updateConversation(currentConversation.id, {
         messages: [],
+        name: '',
       });
     }
   }, [updateConversation]);
